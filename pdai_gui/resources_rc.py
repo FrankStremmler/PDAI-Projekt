66f368057cd7f9f6434b720c9c3a75aa309e0bec
Frank Stremmler <120681982+FrankStremmler@users.noreply.github.com>
2026-06-02 22:27:03 +0200

    collapse.svg korrigiert

--- pdai_gui/resources_rc.py
+++ pdai_gui/resources_rc.py
@@ -61,26 +61,334 @@ roke-width=\x224\x22 s\
 troke-linecap=\x22r\
 ound\x22/>\x0d\x0a</svg>\x0d\
 \x0a\
-\x00\x00\x01\x17\
+\x00\x00\x14Y\
 <\
-svg xmlns=\x22http:\
-//www.w3.org/200\
-0/svg\x22 viewBox=\x22\
-0 0 64 64\x22>\x0d\x0a  <\
-path d=\x22M16 32h3\
-2\x22 stroke=\x22#3741\
-51\x22 stroke-width\
-=\x226\x22 stroke-line\
-cap=\x22round\x22/>\x0d\x0a \
- <path d=\x22M34 18\
-l12 14-12 14\x22 fi\
-ll=\x22none\x22 stroke\
-=\x22#374151\x22 strok\
-e-width=\x226\x22 stro\
-ke-linecap=\x22roun\
-d\x22 stroke-linejo\
-in=\x22round\x22/>\x0d\x0a</\
-svg>\x0d\x0a\
+?xml version=\x221.\
+0\x22 encoding=\x22UTF\
+-8\x22 standalone=\x22\
+no\x22?>\x0a<svg xmlns\
+=\x22http://www.w3.\
+org/2000/svg\x22\x0a  \
+   xmlns:svg=\x22ht\
+tp://www.w3.org/\
+2000/svg\x22\x0a     v\
+ersion=\x221.1\x22\x0a   \
+  width=\x220.21333\
+3in\x22 height=\x220.2\
+13333in\x22\x0a     vi\
+ewBox=\x220 0 64 64\
+\x22>\x0a  <defs>\x0a  </\
+defs>\x0a  <image i\
+d=\x22raster0\x22\x0a    \
+     x=\x220\x22\x0a     \
+    y=\x220\x22\x0a      \
+   width=\x2264\x22\x0a  \
+       height=\x226\
+4\x22\x0a         opac\
+ity=\x221,000000\x22\x0a \
+        href=\x22da\
+ta:image/png;bas\
+e64,iVBORw0KGgoA\
+AAANSUhEUgAAAEAA\
+AABACAYAAACqaXHe\
+AAAMfmVYSWZJSSoA\
+CAAAAAAADgAAAAkA\
+/gAEAAEAAAABAAAA\
+AAEEAAEAAAAAAQAA\
+AQEEAAEAAAAAAQAA\
+AgEDAAMAAACAAAAA\
+AwEDAAEAAAAGAAAA\
+BgEDAAEAAAAGAAAA\
+FQEDAAEAAAADAAAA\
+AQIEAAEAAACGAAAA\
+AgIEAAEAAAD4CwAA\
+AAAAAAgACAAIAP/Y\
+/+AAEEpGSUYAAQEA\
+AAEAAQAA/9sAQwAI\
+BgYHBgUIBwcHCQkI\
+CgwUDQwLCwwZEhMP\
+FB0aHx4dGhwcICQu\
+JyAiLCMcHCg3KSww\
+MTQ0NB8nOT04Mjwu\
+MzQy/9sAQwEJCQkM\
+CwwYDQ0YMiEcITIy\
+MjIyMjIyMjIyMjIy\
+MjIyMjIyMjIyMjIy\
+MjIyMjIyMjIyMjIy\
+MjIyMjIyMjIyMjIy\
+/8AAEQgBAAEAAwEi\
+AAIRAQMRAf/EAB8A\
+AAEFAQEBAQEBAAAA\
+AAAAAAABAgMEBQYH\
+CAkKC//EALUQAAIB\
+AwMCBAMFBQQEAAAB\
+fQECAwAEEQUSITFB\
+BhNRYQcicRQygZGh\
+CCNCscEVUtHwJDNi\
+coIJChYXGBkaJSYn\
+KCkqNDU2Nzg5OkNE\
+RUZHSElKU1RVVldY\
+WVpjZGVmZ2hpanN0\
+dXZ3eHl6g4SFhoeI\
+iYqSk5SVlpeYmZqi\
+o6Slpqeoqaqys7S1\
+tre4ubrCw8TFxsfI\
+ycrS09TV1tfY2drh\
+4uPk5ebn6Onq8fLz\
+9PX29/j5+v/EAB8B\
+AAMBAQEBAQEBAQEA\
+AAAAAAABAgMEBQYH\
+CAkKC//EALURAAIB\
+AgQEAwQHBQQEAAEC\
+dwABAgMRBAUhMQYS\
+QVEHYXETIjKBCBRC\
+kaGxwQkjM1LwFWJy\
+0QoWJDThJfEXGBka\
+JicoKSo1Njc4OTpD\
+REVGR0hJSlNUVVZX\
+WFlaY2RlZmdoaWpz\
+dHV2d3h5eoKDhIWG\
+h4iJipKTlJWWl5iZ\
+mqKjpKWmp6ipqrKz\
+tLW2t7i5usLDxMXG\
+x8jJytLT1NXW19jZ\
+2uLj5OXm5+jp6vLz\
+9PX29/j5+v/aAAwD\
+AQACEQMRAD8A+f6K\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiigAooooAKK\
+KKACiiu++Gnw0/4W\
+J/an/E2/s/7D5X/L\
+t5u/fv8A9pcY2e/W\
+gDgaK9//AOGZf+pu\
+/wDKb/8AbaP+GZf+\
+pu/8pv8A9toA8Aor\
+vviX8NP+Fd/2X/xN\
+v7Q+3eb/AMu3lbNm\
+z/abOd/t0rgaACii\
+igAooooAKKKKACii\
+igAooooAKKKKACii\
+igAooooAKKKKACii\
+igAooooAKKKKACii\
+igAr3/8AZl/5mn/t\
+0/8Aa1eAV7/+zL/z\
+NP8A26f+1qAPoCii\
+igDz/wCJvwy/4WN/\
+Zf8AxN/7P+web/y7\
+ebv37P8AbXGNnv1r\
+5Ar7/rz/AOJvwy/4\
+WN/Zf/E3/s/7B5v/\
+AC7ebv37P9tcY2e/\
+WgD5AooooAKKKKAC\
+iiigAooooAKKKKAC\
+iiigAooooAKKKKAC\
+iiigAooooAKKKKAC\
+iiigAooooAK9A+GX\
+xN/4Vz/an/Eo/tD7\
+f5X/AC8+Vs2b/wDY\
+bOd/t0rz+igD7/or\
+5A+GXxN/4Vz/AGp/\
+xKP7Q+3+V/y8+Vs2\
+b/8AYbOd/t0r6/oA\
+KKKKAPP/AIm/DL/h\
+Y39l/wDE3/s/7B5v\
+/Lt5u/fs/wBtcY2e\
+/WvkCvv+vD/2iNN/\
+tD/hG/3vl7PtX8Oc\
+58r39qaTbsiZzjCP\
+NLY+cKKKKRQUUUUA\
+FFFFABRRRQAUUUUA\
+FFFFABRRRQAUUUUA\
+FFFFABRRRQAUUUUA\
+FFFFABRRRQAV6B8M\
+vib/AMK5/tT/AIlH\
+9ofb/K/5efK2bN/+\
+w2c7/bpXn9FAH3/R\
+Xz/+zL/zNP8A26f+\
+1q+gKACiiigD5A+J\
+vwy/4Vz/AGX/AMTf\
++0Pt/m/8u3lbNmz/\
+AG2znf7dK8/r7/r5\
+A+Jvwy/4Vz/Zf/E3\
+/tD7f5v/AC7eVs2b\
+P9ts53+3SgDz+iii\
+gAooooAKKKKACiii\
+gAooooAKKKKACiii\
+gDvvhp8NP+Fif2p/\
+xNv7P+w+V/y7ebv3\
+7/8AaXGNnv1r0H/h\
+mX/qbv8Aym//AG2j\
+9mX/AJmn/t0/9rV9\
+AUAfP/8AwzL/ANTd\
+/wCU3/7bR/wzL/1N\
+3/lN/wDttfQFFAHz\
+/wD8My/9Td/5Tf8A\
+7bR/wzL/ANTd/wCU\
+3/7bX0BRQB8//wDD\
+Mv8A1N3/AJTf/ttH\
+/DMv/U3f+U3/AO21\
+9AUUAfP/APwzL/1N\
+3/lN/wDttH/DMv8A\
+1N3/AJTf/ttfQFFA\
+Hz//AMMy/wDU3f8A\
+lN/+20f8My/9Td/5\
+Tf8A7bX0BRQB8/8A\
+/DMv/U3f+U3/AO21\
+9AUUUAFFFFABRRRQ\
+B8//APDMv/U3f+U3\
+/wC21598S/hp/wAK\
+7/sv/ibf2h9u83/l\
+28rZs2f7TZzv9ulf\
+YNfP/wC01/zK3/b3\
+/wC0aAPAKKKKACii\
+igAooooAKKKKACii\
+igAooooA9/8A2Zf+\
+Zp/7dP8A2tX0BXx9\
+8NPiX/wrv+1P+JT/\
+AGh9u8r/AJefK2bN\
+/wDstnO/26V6D/w0\
+1/1KP/lS/wDtVAH0\
+BRXz/wD8NNf9Sj/5\
+Uv8A7VR/w01/1KP/\
+AJUv/tVAH0BRXz//\
+AMNNf9Sj/wCVL/7V\
+R/w01/1KP/lS/wDt\
+VAH0BRXz/wD8NNf9\
+Sj/5Uv8A7VR/w01/\
+1KP/AJUv/tVAH0BR\
+Xz//AMNNf9Sj/wCV\
+L/7VR/w01/1KP/lS\
+/wDtVAH0BRXz/wD8\
+NNf9Sj/5Uv8A7VR/\
+w01/1KP/AJUv/tVA\
+H0BRXz//AMNNf9Sj\
+/wCVL/7VX0BQAUUU\
+UAFFFFABXz/+01/z\
+K3/b3/7Ro/4aa/6l\
+H/ypf/aq8++JfxL/\
+AOFif2X/AMSn+z/s\
+Pm/8vPm79+z/AGVx\
+jZ79aAOBooooAKKK\
+KACiiigAooooAKKK\
+KACiiigAooooAKKK\
+KACiiigAooooAKKK\
+KACiivQPhl8Mv+Fj\
+f2p/xN/7P+weV/y7\
+ebv37/8AbXGNnv1o\
+A7/9mX/maf8At0/9\
+rV9AUUUAFFFFABXy\
+B8Tfib/wsb+y/wDi\
+Uf2f9g83/l583fv2\
+f7C4xs9+tHxN+Jv/\
+AAsb+y/+JR/Z/wBg\
+83/l583fv2f7C4xs\
+9+tef0AFFFFABRRR\
+QAUUUUAFFFFABRRR\
+QAUUUUAFFFFABRRR\
+QAUUUUAFFFFABRRR\
+QAUUV6B8Mvhl/wAL\
+G/tT/ib/ANn/AGDy\
+v+Xbzd+/f/trjGz3\
+60AHwy+GX/Cxv7U/\
+4m/9n/YPK/5dvN37\
+9/8AtrjGz3619f0U\
+UAFFFFABXh/7RGpf\
+2f8A8I3+68zf9q/i\
+xjHle3vXYfE34m/8\
+K5/sv/iUf2h9v83/\
+AJefK2bNn+w2c7/b\
+pXyBTTad0TOEZx5Z\
+bBRRRSKCiiigAooo\
+oAKKKKACiiigAooo\
+oAKKKKACiiigAooo\
+oAKKKKACiiigAooo\
+oAK9/wD2Zf8Amaf+\
+3T/2tXgFe/8A7Mv/\
+ADNP/bp/7WoA+gKK\
+KKACvP8A4m/E3/hX\
+P9l/8Sj+0Pt/m/8A\
+Lz5WzZs/2Gznf7dK\
+Pib8Tf8AhXP9l/8A\
+Eo/tD7f5v/Lz5WzZ\
+s/2Gznf7dK+QKACi\
+iigAooooAKKKKACi\
+iigAooooAKKKKACi\
+iigAooooAKKKKACi\
+iigAooooAKKKKACi\
+iigArvvhp8S/+Fd/\
+2p/xKf7Q+3eV/wAv\
+PlbNm/8A2Wznf7dK\
+4GigD3//AIaa/wCp\
+R/8AKl/9qo/4aa/6\
+lH/ypf8A2qvAKKAO\
+++JfxL/4WJ/Zf/Ep\
+/s/7D5v/AC8+bv37\
+P9lcY2e/WuBoooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooAK\
+KKKACiiigAooooA/\
+/9kGCidIAAAAAXNS\
+R0IB2cksfwAAAARn\
+QU1BAACxjwv8YQUA\
+AAAgY0hSTQAAeiYA\
+AICEAAD6AAAAgOgA\
+AHUwAADqYAAAOpgA\
+ABdwnLpRPAAAAPxJ\
+REFUeNrtmDESgzAM\
+BC3yLfIM+P8rMFWU\
+JlWGzADB8mHvljT4\
+FhlZTgkAAAAAAAAA\
+IllyHv2LJecxcg1W\
+U4C7++aizIaUkkes\
+YVCsDHd/Rb1rUN0e\
+v6qjGwFREqQFREiw\
+uwQ0M2tSQG0JEltg\
+d7BpGpqsgCOVcHUV\
+SAmoIUGuC3xOgWGd\
+QbEN+lWVcsstEN0Z\
+TlXA1hR3NUdE/TNF\
+WsmvI9tO73QU7n4W\
+QICigLyuT7UgZ9fU\
+fRu0nsOr/gMsKjzD\
+EOOw0BbYFX6eH01e\
+iNS8EuNStOfwtzgK\
+lwwvL6B0eGkBEeFl\
+Bey5GG1CwNYE93nm\
+CQAAAAAAAABK8QbQ\
+kukpZHsR4QAAAABJ\
+RU5ErkJggqo=\x22 />\
+\x0a</svg>\x0a\
 \x00\x00\x01\xc0\
 <\
 svg xmlns=\x22http:\
@@ -252,21 +560,21 @@ qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x09\x00\x00\x00\x02\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\xd2\x00\x00\x00\x00\x00\x01\x00\x00\x09\x06\
+\x00\x00\x00\xd2\x00\x00\x00\x00\x00\x01\x00\x00\x1cH\
 \x00\x00\x01\x9e\x89\xe1\xc9H\
-\x00\x00\x00\xe8\x00\x00\x00\x00\x00\x01\x00\x00\x09\xe3\
+\x00\x00\x00\xe8\x00\x00\x00\x00\x00\x01\x00\x00\x1d%\
 \x00\x00\x01\x9e\x89\xe1\xc9H\
-\x00\x00\x00\x5c\x00\x00\x00\x00\x00\x01\x00\x00\x048\
+\x00\x00\x00\x5c\x00\x00\x00\x00\x00\x01\x00\x00\x17z\
 \x00\x00\x01\x9e\x89\xe1\xc9H\
-\x00\x00\x00z\x00\x00\x00\x00\x00\x01\x00\x00\x05\xfc\
+\x00\x00\x00z\x00\x00\x00\x00\x00\x01\x00\x00\x19>\
 \x00\x00\x01\x9e\x89\xe1\xc9H\
 \x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
 \x00\x00\x01\x9e\x89\xe1\xc9H\
 \x00\x00\x00>\x00\x00\x00\x00\x00\x01\x00\x00\x03\x1d\
-\x00\x00\x01\x9e\x89\xf3\x18\xef\
-\x00\x00\x00\xb2\x00\x00\x00\x00\x00\x01\x00\x00\x07\xcf\
+\x00\x00\x01\x9e\x8a\x01\xbf\x93\
+\x00\x00\x00\xb2\x00\x00\x00\x00\x00\x01\x00\x00\x1b\x11\
 \x00\x00\x01\x9e\x89\xe1\xc9H\
-\x00\x00\x00\x98\x00\x00\x00\x00\x00\x01\x00\x00\x06\xb9\
+\x00\x00\x00\x98\x00\x00\x00\x00\x00\x01\x00\x00\x19\xfb\
 \x00\x00\x01\x9e\x89\xe4\x08\xf6\
 \x00\x00\x00(\x00\x00\x00\x00\x00\x01\x00\x00\x01w\
 \x00\x00\x01\x9e\x89\xea\x1a\xae\
